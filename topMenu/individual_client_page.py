ef0bcdb2dc2b8de85e0b4fbafcfafbdd7c9a9c55
oskarku <oskar.kufel@smcebi.us.edu.pl>
2024-02-24 18:50:20 +0100

    implement :  . checkSubmenuCountElement,    validNameElementInSubmenu,    checkTitlePage,    readeElementInMenu,

--- topMenu/individual_client_page.py
+++ topMenu/individual_client_page.py
@@ -2,17 +2,73 @@
 This code include  methode to test pages like as
 chckSubmenu, breadcrumb_item
 """
+from telnetlib import EC
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 class accountBankingTestPage():
 
     def __init__(self, driver):
         self.driver = driver
 
-    def checkSubmenu(self,listOfStringBreadcrumbsInnerElement):
-        return False
+    def testAllPages(self):
+        pass
 
-    def checkTitleMenu(self,validText):
-        return False
+    def checkSubmenuCountElement(self,listOfStringBreadcrumbsInnerElement):
+        isCheckFlagSubmenuCountElement=False
+        try:
+            elementsSubMenu = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.ID, "breadcrumb_item"))
+            )
+            if len(listOfStringBreadcrumbsInnerElement)==len(elementsSubMenu):
+                isCheckFlagSubmenuCountElement=True
+
+        except Exception as e:
+            print("Błąd podczas oczekiwania na element:", str(e))
+
+        return isCheckFlagSubmenuCountElement
+
+
+    def validNameElementInSubmenu(self,listOfStringBreadcrumbsInnerElement):
+        isValidNameElementInSubmenu=False
+        try:
+            elementsSubMenu = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.ID, "breadcrumb_item"))
+            )
+            for x in len(elementsSubMenu):
+                if elementsSubMenu[x]==listOfStringBreadcrumbsInnerElement[x]:
+                    isValidNameElementInSubmenu=True
+                else:
+                    isValidNameElementInSubmenu=False
+        except Exception as e:
+            print("Błąd podczas oczekiwania na element:", str(e))
+
+        return isValidNameElementInSubmenu
+
+    def checkTitlePage(self, validTitle):
+        isCheckTitlePage=False
+        try:
+            elementTitlePage = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "breadcrumb_item"))
+            )
+            if validTitle==elementTitlePage:
+                isCheckTitlePage=True
+        except Exception as e:
+            print("Błąd podczas oczekiwania na element:", str(e))
+        return isCheckTitlePage
+
+    def readeElementInMenu(self):
+        try:
+            elementTitlePage = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "breadcrumb_item"))
+            )
+        except Exception as e:
+            print("Błąd podczas oczekiwania na element:", str(e))
+
+        return {}
+
 
     def getTestRaportMenu(self,listDictWithInformationdDistroTileBodyElement):
         return{}
