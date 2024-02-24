0e120b737fd2807e6468eacbfcf222f9a04ac48c
oskarku <oskar.kufel@smcebi.us.edu.pl>
2024-02-24 14:53:07 +0100

    Initial commit

---
+++ topMenu/individual_client_page.py
@@ -0,0 +1,42 @@
+"""This is test page  individual page accont banking https://www.ing.pl/indywidualni/konta-osobiste
+This code include  methode to test pages like as
+chckSubmenu, breadcrumb_item
+"""
+
+class accountBankingTestPage():
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def checkSubmenu(self,listOfStringBreadcrumbsInnerElement):
+        return False
+
+    def checkTitleMenu(self,validText):
+        return False
+
+    def getTestRaportMenu(self,listDictWithInformationdDistroTileBodyElement):
+        return{}
+
+    def getRaportTestGreySection(self, listDictElement):
+        return{}
+
+    def getRaportTestSectionWithImage(selfs,listelementWithImage):
+        return {}
+
+    def chceckSectionTitleText(self,stringValidTitle):
+        return False
+
+    def getRaportTestSectionText(self, listOfDictTextFormula):
+        return {}
+
+    def sectionInfo(self, listOfDictElement):
+        return {}
+
+#################################################################################
+
+
+
+
+
+
+
